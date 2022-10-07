205ea7fb1f16042c7130eede749a8f7617cf92a2
AlaaAAmin <alaaamin_epm@yahoo.com>
2022-10-07 11:15:58 +0200

    updated the ssh firewall to allow access through IAP only

--- network/firewall_rules.tf
+++ network/firewall_rules.tf
@@ -8,6 +8,6 @@ resource "google_compute_firewall" "ssh-rule" {
     protocol = "tcp"
     ports    = ["22"]
   }
-  source_ranges = ["0.0.0.0/0"]
+  source_ranges = ["35.235.240.0/20"] # restricting access to IAP
   target_tags   = ["test-vm"]
 }
